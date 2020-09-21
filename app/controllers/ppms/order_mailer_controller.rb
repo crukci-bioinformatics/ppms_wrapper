9b1fc3b2dbe66d4a4340e6b2b85f53e9e0b78908
Richard Bowers <richard.bowers@cruk.cam.ac.uk>
2020-09-21 16:30:58 +0100

    Formulating maps and division by issue.

--- app/controllers/ppms/order_mailer_controller.rb
+++ app/controllers/ppms/order_mailer_controller.rb
@@ -4,6 +4,24 @@ class Ppms::OrderMailerController < ApplicationController
 
   def index
     @orders = TimeEntryOrder.where(mailed_at: nil)
+    #@orders.each do |order|
+    #  $ppmslog.warn("TEO #{order.id} is issue #{order.time_entry.issue.id} - #{order.time_entry.issue.subject}")
+    #end
+    
+    @issues = Hash.new
+    @orders_by_issue = Hash.new
+    
+    @orders.each do |order|
+      issue = order.time_entry.issue
+      @issues[issue.id] = issue
+      
+      current = @orders_by_issue[issue.id]
+      if current.nil?
+        current = Array.new
+        @orders_by_issue[issue.id] = current
+      end
+      current << order
+    end
   end
 
 end
