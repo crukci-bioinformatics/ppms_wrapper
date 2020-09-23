94a7ba062d9fe9becff0b82d07cf0adc21f69461
Richard Bowers <richard.bowers@cruk.cam.ac.uk>
2020-09-23 09:42:39 +0100

    Added root setting.

--- app/controllers/ppms/order_mailer_controller.rb
+++ app/controllers/ppms/order_mailer_controller.rb
@@ -1,12 +1,23 @@
+require 'ppms/utils'
+
 class Ppms::OrderMailerController < ApplicationController
 
+  include PPMS::Utilities
+  
   unloadable
 
   def index
     @researcher_field = CustomField.where(name: "Researcher Email").take!
     @experiment_type_field = CustomField.where(name: "Experiment Type").take!
 
+    mailing_projects = collectProjects(Setting.plugin_ppms['mailing_root'])
+    $ppmslog.info("Filters: #{mailing_projects}")
+
     @orders = TimeEntryOrder.where(mailed_at: nil)
+    $ppmslog.info("Orders before filter: #{@orders.size}")
+    @orders = @orders.select { |order| mailing_projects.include? order.time_entry.project.id }
+    $ppmslog.info("Orders after filter: #{@orders.size}")
+         
     #@orders.each do |order|
     #  $ppmslog.warn("TEO #{order.id} is issue #{order.time_entry.issue.id} - #{order.time_entry.issue.subject}")
     #end
